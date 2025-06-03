ff296c8a9d06ddc0193715092e604a2c154b70a9
devjahid1 <jahidmiah2002@gmail.com>
2025-06-04 05:32:40 +0600

    problem solve by menu

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -13,7 +13,7 @@ const Menu = () => {
             <MenuAbout />
 
             {/* Horizontal layout: left menu, right content */}
-            <div className="w-full max-w-screen-[1440px] pl-[99px] flex items-center justify-center gap-40 mt-8">
+            <div className="w-full max-w-screen-[1440px] pl-[99px] flex items-center justify-center gap-28 mt-8">
                 <div className="w-1/4">
                     <MainMenu />
                 </div>
